3caa6142e3156b10215ea9fc0ea600ce167d7b86
Elise Colin <ecolin@student.42.fr>
2023-04-27 23:57:57 +0200

    Localhost connection successfull

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: elise <elise@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 12:22:32 by elise             #+#    #+#             */
-/*   Updated: 2023/04/25 16:02:53 by elise            ###   ########.fr       */
+/*   Updated: 2023/04/27 23:54:47 by elise            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,30 @@ int main(int argc, char *argv[])
 
     struct addrinfo *res;
     errorin(getaddrinfo(0, port, &hints, &res) != 0, "Failed to get address info.");
-    errorin(bind(listen_socket, res->ai_addr, res->ai_addrlen) == -1, "Failed to bind listen socket.");
+    errorin(bind(listen_socket, res->ai_addr, res->ai_addrlen) == -1, "Failed to bind listen socket.");//associates socket with local port
     freeaddrinfo(res);
     errorin(listen(listen_socket, SOMAXCONN) == -1, "Failed to listen on socket.");
     std::cout << "Listening on port " << port << "..." << std::endl;
+    while (true)
+    {
+        struct sockaddr client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        int client_socket = accept(listen_socket, &client_addr, &client_addr_len);
+        if (client_socket == -1)
+        {
+            std::cerr << "Failed to accept incoming connection.\n";
+            continue;
+        }
+        std::cout << "New connection successfull!\n";
+    }
+    // char buffer[1024];
+    // int bytes_received = recv(client_socket, buffer, sizeof buffer, 0);
+        // if (bytes_received == -1) {
+            // std::cerr << "Failed to receive data from client." << std::endl;
+            // close(client_socket);
+            // continue;
+        // }
     // struct sockaddr_in *addr_in = (struct sockaddr_in*)(res->ai_addr);
     // std::cout << "IP address: " << inet_ntoa(addr_in->sin_addr) << std::endl;
 }
